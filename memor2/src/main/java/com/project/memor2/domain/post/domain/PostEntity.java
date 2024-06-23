3d42a519d95d403577159dbbeef6ff05b3829b22
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-06-24 00:27:46 +0900

    postentity

---
+++ memor2/src/main/java/com/project/memor2/domain/post/domain/PostEntity.java
@@ -0,0 +1,37 @@
+package com.project.memor2.domain.post.domain;
+
+import com.project.memor2.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@SuperBuilder
+@DynamicUpdate
+@Table(name = "tb_user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String author;
+
+}
