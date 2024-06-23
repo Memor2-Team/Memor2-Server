2767880c9c3affc0f65cb253e26006ee4d5a13cc
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-06-23 15:36:57 +0900

    re server setting

---
+++ src/main/java/com/project/memor2/Memor2Application.java
@@ -0,0 +1,15 @@
+package com.project.memor2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class Memor2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Memor2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/memor2/Memor2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.memor2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Memor2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
